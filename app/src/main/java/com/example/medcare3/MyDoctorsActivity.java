d3b254bf0a760d893f5c04f7a289f9378b5a2f85
Madaline Albright <fenesa111@gmail.com>
2021-04-15 01:01:11 +0300

    second commit

---
+++ app/src/main/java/com/example/medcare3/MyDoctorsActivity.java
@@ -0,0 +1,14 @@
+package com.example.medcare3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyDoctorsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_doctors);
+    }
+}
