4d9bd6465c83733bb32d6b897e2602b8bc97a193
Madaline Albright <fenesa111@gmail.com>
2021-04-14 23:40:48 +0300

    AppointmentActivity added

---
+++ app/src/main/java/com/example/medcare3/database/List_Data.java
@@ -0,0 +1,22 @@
+package com.example.medcare3.database;
+
+public class List_Data {
+   // private String id;
+    private String time;
+   // private String movie;
+
+public List_Data(){
+
+}
+
+    public List_Data( String time ) {
+     //   this.id = id;
+        this.time = time;
+    }
+
+
+    public String getTime() {
+        return time;
+    }
+
+}
---
+++ app/src/main/java/com/example/medcare3/slotbook/slot.java
@@ -0,0 +1,93 @@
+package com.example.medcare3.slotbook;
+
+public class slot {
+
+    private String name;
+    private String dob;
+    private String phoneno;
+    private String timeInterval;
+    private String time;
+
+    public slot() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getPhoneno() {
+        return phoneno;
+    }
+
+    public void setPhoneno(String phoneno) {
+        this.phoneno = phoneno;
+    }
+
+    public String getTimeInterval() {
+        return timeInterval;
+    }
+
+    public void setTimeInterval(String timeInterval) {
+        this.timeInterval = timeInterval;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getDob() {
+//        return dob;
+//    }
+//
+//    public void setDob(String dob) {
+//        this.dob = dob;
+//    }
+//
+//    public String getPhoneno() {
+//        return phoneno;
+//    }
+//
+//    public void setPhoneno(String phoneno) {
+//        this.phoneno = phoneno;
+//    }
+//
+//    public String getTimeInterval() {
+//        return timeInterval;
+//    }
+//
+//    public void setTimeInterval(String timeInterval) {
+//        this.timeInterval = timeInterval;
+//    }
+//
+//    public String getTime() {
+//        return time;
+//    }
+//
+//    public void setTime(String time) {
+//        this.time = time;
+//    }
+}
